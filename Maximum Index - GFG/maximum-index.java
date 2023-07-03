8fd81833a200fc3ff6d3214f5f18d41dc2fd0d04
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-07-03 13:21:57 +0530

    Added solution - LeetHub

--- Maximum Index - GFG/maximum-index.java
+++ Maximum Index - GFG/maximum-index.java
@@ -25,27 +25,27 @@ class GFG {
 class Solution {
 
     int maxIndexDiff(int arr[], int n) {
-         int min[]=new int[n];
-        int max[]=new int[n];
-        min[0]=arr[0];
-        max[n-1]=arr[n-1];
-        for(int i=1;i<n;i++){
-            min[i]=Math.min(min[i-1],arr[i]);
-        }
-        for(int j=n-2;j>=0;j--){
-            max[j]=Math.max(max[j+1],arr[j]);
-        }
-        int res=0;
-        int i=0,j=0;
-        while(i<n && j<n){
-            if(min[i]<=max[j]){
-                res=Math.max(res,j-i);
+        int right[] = new int[n]; //max, j
+        int left[] = new int[n];//min, i
+        left[0] = arr[0];  right[n - 1] = arr[n - 1];
+
+        for (int i = 1; i < n; i++) 
+        left[i] = Math.min(arr[i], left[i - 1]);
+
+        for (int j = n - 2; j >= 0; j--)
+            right[j] = Math.max(arr[j], right[j + 1]);
+
+        int i = 0, j = 0, ans = Integer.MIN_VALUE;
+        while (j < n && i < n) 
+        {
+            if (left[i] <= right[j]) 
+            {
+                ans = Math.max(ans, j - i);
                 j++;
-            }
-            else{
+            } 
+            else
                 i++;
-            }
         }
-        return res;
+        return ans;
     }
 }
