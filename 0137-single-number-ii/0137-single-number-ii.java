873f9aac6e8f32f0b4e603b5f511fe415dd9fb68
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-07-04 11:37:33 +0530

    Time: 0 ms (100.00%), Space: 43.8 MB (71.19%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,22 +1,13 @@
 // Time Complexity : O(N)
-// Space Complexity : O(N) using HashMap
+// Space Complexity : O(1) using Bit Manipulation
 class Solution {
     public int singleNumber(int[] nums) {
-        int res = 0;
-        Map<Integer, Integer> mp = new HashMap<>();
-
-        for (int i : nums) 
-        {
-            if (mp.containsKey(i)) 
-                mp.put(i, mp.get(i) + 1);
-            else 
-                mp.put(i, 1);
-        }
-        for (Map.Entry<Integer, Integer> e : mp.entrySet()) 
-        {
-            if(e.getValue()==1)
-            res=e.getKey();
-        }
-        return res;
+        int ones = 0, twos = 0;
+    for (int i = 0; i < nums.length; i++) 
+    {
+        ones = (ones ^ nums[i]) & ~twos;
+        twos = (twos ^ nums[i]) & ~ones;
+    }
+    return ones;    
     }
 }
