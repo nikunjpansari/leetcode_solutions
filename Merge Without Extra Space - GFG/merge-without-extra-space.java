8cc8dfc57ae4193be5cf733484621de3f2c05376
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-07-07 15:28:31 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.java
+++ Merge Without Extra Space - GFG/merge-without-extra-space.java
@@ -48,30 +48,48 @@ public class Main
 class Solution
 {
     //Function to merge the arrays.
-    public static void merge(long arr1[], long arr2[], int n, int m) 
-    {
-      
-        int i = 0;
-        int last = n-1;
-        int j = 0;
-        while(i<n && j<m){
-            if(arr1[i]>arr2[j]){
-                long temp = arr1[last];
-                arr1[last] = arr2[j];
-                arr2[j] = temp;
-                
-                j++;
-                last--;
-            
-            }else{
-                i++;
-            }
-           
-            
-     
+  public static void swapIfGreater(long[] arr1, long[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            long temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
         }
-        
-         Arrays.sort(arr1);
-         Arrays.sort(arr2);
     }
+
+    public static void merge(long[] arr1, long[] arr2, int n, int m) {
+
+        // len of the imaginary single array:
+        int len = n + m;
+
+        // Initial gap:
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            // Place 2 pointers:
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                // case 1: left in arr1[]
+                //and right in arr2[]:
+                if (left < n && right >= n) {
+                    swapIfGreater(arr1, arr2, left, right - n);
+                }
+                // case 2: both pointers in arr2[]:
+                else if (left >= n) {
+                    swapIfGreater(arr2, arr2, left - n, right - n);
+                }
+                // case 3: both pointers in arr1[]:
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++; right++;
+            }
+            // break if iteration gap=1 is completed:
+            if (gap == 1) break;
+
+            // Otherwise, calculate new gap:
+            gap = (gap / 2) + (gap % 2);
+        }
+    }
+
 }
