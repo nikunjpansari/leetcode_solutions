68a75e779d4e4cacf8920539d4cf94f375a8698b
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-07-11 16:11:40 +0530

    Added solution - LeetHub

---
+++ Length of the longest substring - GFG/length-of-the-longest-substring.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String s = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestUniqueSubsttr(s));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution{
+    int longestUniqueSubsttr(String s){
+   
+        HashMap < Character, Integer > mpp = new HashMap < Character, Integer > ();
+
+        int left = 0, right = 0;
+        int n = s.length();
+        int len = 0;
+        while (right < n) {
+            if (mpp.containsKey(s.charAt(right))) left = Math.max(mpp.get(s.charAt(right)) + 1, left);
+
+            mpp.put(s.charAt(right), right);
+
+            len = Math.max(len, right - left + 1);
+            right++;
+        }
+        return len;
+    }
+}
