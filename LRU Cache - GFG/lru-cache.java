e56ea2cead2e3ef7763f27a63e51cc0f520ab683
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-07-18 12:29:07 +0530

    Added solution - LeetHub

---
+++ LRU Cache - GFG/lru-cache.java
@@ -0,0 +1,100 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+  public class LRUDesign {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+
+            int capacity = Integer.parseInt(read.readLine());
+            int queries = Integer.parseInt(read.readLine());
+            LRUCache cache = new LRUCache(capacity);
+            String str[] = read.readLine().trim().split(" ");
+            int len = str.length;
+            int itr = 0;
+
+            for (int i = 0; (i < queries) && (itr < len); i++) {
+                String queryType = str[itr++];
+                if (queryType.equals("SET")) {
+                    int key = Integer.parseInt(str[itr++]);
+                    int value = Integer.parseInt(str[itr++]);
+                    cache.set(key, value);
+                }
+                if (queryType.equals("GET")) {
+                    int key = Integer.parseInt(str[itr++]);
+                    System.out.print(cache.get(key) + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// design the class in the most optimal way
+
+
+class LRUCache {  
+  Node head = new Node(0, 0), tail = new Node(0, 0);
+  Map<Integer, Node> map = new HashMap();
+  int capacity;
+  
+  public LRUCache(int _capacity) {
+    capacity = _capacity;
+    head.next = tail;
+    tail.prev = head;
+  }
+
+  public int get(int key) {
+    if(map.containsKey(key)) {
+      Node node = map.get(key);
+      remove(node);
+      insert(node);
+      return node.value;
+    } else {
+      return -1;
+    }
+  }
+
+  public void set(int key, int value) {
+    if(map.containsKey(key)) {
+      remove(map.get(key));
+    }
+    if(map.size() == capacity) {
+      remove(tail.prev);
+    }
+    insert(new Node(key, value));
+  }
+  
+  private void remove(Node node) {
+    map.remove(node.key);
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
+  
+  private void insert(Node node){
+    map.put(node.key, node);
+    node.next = head.next;
+    node.next.prev = node;
+    head.next = node;
+    node.prev = head;
+  }
+  
+  class Node{
+    Node prev, next;
+    int key, value;
+    Node(int _key, int _value) {
+      key = _key;
+      value = _value;
+    }
+  }
+}
