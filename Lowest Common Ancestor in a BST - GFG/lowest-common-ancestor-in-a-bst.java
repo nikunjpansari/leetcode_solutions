25dc35d637a7ab52a9a99ee7101dfb396ba94937
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-07-28 14:01:35 +0530

    Added solution - LeetHub

--- Lowest Common Ancestor in a BST - GFG/lowest-common-ancestor-in-a-bst.java
+++ Lowest Common Ancestor in a BST - GFG/lowest-common-ancestor-in-a-bst.java
@@ -122,15 +122,15 @@ class BST
     //Function to find the lowest common ancestor in a BST. 
 	Node LCA(Node root, int n1, int n2)
 	{
-        if(root == null) 
-        return null; 
-        
-        if(n1 > root.data && n2 > root.data) 
-            return LCA(root.right, n1, n2);
-        else if(root.data > n1 && root.data > n2)
-            return LCA(root.left, n1, n2);
-        
-        return root;  
+         while(true){
+            if(root.data>n1 && root.data>n2){
+                root=root.left;
+            }
+            else if(root.data<n1 && root.data<n2){
+                root=root.right;
+            }
+            else return root;
+        }
     }
     
 }
