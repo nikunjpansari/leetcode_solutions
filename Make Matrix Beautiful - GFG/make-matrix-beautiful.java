a4e4ae0760b646c69654f21a17031b319ec8f048
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-08-22 01:56:43 +0530

    Added solution - LeetHub

---
+++ Make Matrix Beautiful - GFG/make-matrix-beautiful.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntMatrix
+{
+    public static int[][] input(BufferedReader br, int n, int m) throws IOException
+    {
+        int[][] mat = new int[n][];
+
+        for(int i = 0; i < n; i++)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            mat[i] = new int[s.length];
+            for(int j = 0; j < s.length; j++)
+                mat[i][j] = Integer.parseInt(s[j]);
+        }
+
+        return mat;
+    }
+
+    public static void print(int[][] m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+
+    public static void print(ArrayList<ArrayList<Integer>> m)
+    {
+        for(var a : m)
+        {
+            for(int e : a)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N;
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[][] matrix = IntMatrix.input(br, N, N);
+            
+            Solution obj = new Solution();
+            int res = obj.findMinOperation(N, matrix);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findMinOperation(int n, int[][] arr) {
+       int tsum = 0, rmax = 0, cmax = 0;
+        for(int i = 0;i<n;i++){
+            int rsum = 0, csum = 0;
+            for(int j = 0;j<n;j++){
+                rsum += arr[i][j];
+                csum += arr[j][i];
+            }
+            rmax = Math.max(rmax, rsum);
+            cmax = Math.max(cmax, csum);
+            tsum += rsum;
+        }
+        return Math.max(cmax, rmax)*n-tsum;
+    }
+}
+        
