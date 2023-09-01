bf1d0c2879241f2616254373b1f70952987386a1
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-01 14:43:48 +0530

    Added solution - LeetHub

--- Level order traversal - GFG/level-order-traversal.java
+++ Level order traversal - GFG/level-order-traversal.java
@@ -109,6 +109,8 @@ class GfG {
 // } Driver Code Ends
 
 
+
+
 //User function Template for Java
 
 /*
@@ -127,32 +129,35 @@ class Node
 class Solution
 {
     //Function to return the level order traversal of a tree.
-     public static ArrayList<Integer> levelOrder(Node root) {
-        ArrayList<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
+     public static ArrayList<Integer> levelOrder(Node node) 
+     {
+        ArrayList<Integer> res = new ArrayList<>();
+      
+        Queue<Node> q = new LinkedList<Node>();
+        Node root = node;
+        q.add(root);
+        
+         if (root == null) 
+            return res;
+            
+        while(!q.isEmpty())
+        {
+            int n = q.size();
+            for(int i=0; i<n ; i++)
+            {
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(root);
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            for (int i = 0; i < levelSize; i++) {
-                Node node = queue.poll();
-                result.add(node.data);
-
-                if (node.left != null) {
-                    queue.offer(node.left);
-                }
-
-                if (node.right != null) {
-                    queue.offer(node.right);
-                }
+            Node temp = q.poll();
+            res.add(temp.data);
+            
+            if(temp.left!=null)
+            q.add(temp.left);
+            
+            if(temp.right!=null)
+            q.add(temp.right);
             }
+           
         }
-
-        return result;
+        return res;
     }
 
 }
