6b4e2d530472b3ef1e0f9f0d3fdbd5cfa77f8803
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-01 17:09:54 +0530

    Added solution - LeetHub

---
+++ Merge Sort for Linked List - GFG/merge-sort-for-linked-list.java
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class Driverclass
+{
+    
+    public static void main (String[] args) 
+    {
+        Scanner sc= new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            while(n-- > 1){
+		        tail.next = new Node(sc.nextInt());
+		        tail = tail.next;
+		    }
+		   
+		      head = new Solution().mergeSort(head);
+		     printList(head);
+		    System.out.println();
+        }
+    }
+    public static void printList(Node head)
+    {
+        if(head == null)
+           return;
+           
+        Node temp = head;
+        while(temp != null)
+        {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        this.data = key;
+        next = null;
+    }
+} */
+
+class Solution
+{
+    //Function to sort the given linked list using Merge Sort.
+    static Node mergeSort(Node head)
+    {
+        Node temp=head;
+        int count=0;
+        while(temp!=null)
+        {
+            count++;
+            temp=temp.next;
+        }
+        temp=head;
+        int a[]=new int[count];
+        count=0;
+        while(temp!=null)
+        {
+            a[count++]=temp.data;
+            temp=temp.next;
+        }
+        temp=head;
+        Arrays.sort(a);
+        int k=0;
+        while(temp!=null){
+            temp.data=a[k++];
+            temp=temp.next;
+        }
+        return head;
+    }
+}
+
+
