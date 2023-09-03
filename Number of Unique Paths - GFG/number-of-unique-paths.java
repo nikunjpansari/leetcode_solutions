e3b66c14d9bc50ce69fcb9b55a9aeef43a320da7
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-03 14:19:26 +0530

    Added solution - LeetHub

---
+++ Number of Unique Paths - GFG/number-of-unique-paths.java
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking dimensions of the matrix
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    Solution ob = new Solution();
+		    //calling method NumberOfPath()
+		    System.out.println(ob.NumberOfPath(a,b));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find total number of unique paths.
+    public static int NumberOfPath(int m, int n) 
+    {
+         if(m == 1 || n == 1)
+            return 1;
+        m--;
+        n--;
+        if(m < n) {            
+ 
+            m = m + n;
+            n = m - n;
+            m = m - n;
+        }
+        long res = 1;
+        int j = 1;
+        for(int i = m+1; i <= m+n; i++, j++)
+        {      
+            res *= i;
+            res /= j;
+        }
+            
+        return (int)res;
+    }
+}
