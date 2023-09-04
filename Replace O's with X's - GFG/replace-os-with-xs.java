7cdd4d9da626079197d64c273f1eeb293c1cda21
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-04 12:23:56 +0530

    Added solution - LeetHub

--- Replace O's with X's - GFG/replace-os-with-xs.java
+++ Replace O's with X's - GFG/replace-os-with-xs.java
@@ -40,66 +40,62 @@ class GFG{
 
 
 //User function Template for Java
+class Solution {
+    int[] dx = {-1, 0, 1, 0};
+    int[] dy = {0, 1, 0, -1};
 
-class Solution{
-   static void dfs(int row, int col,int vis[][], 
-    char mat[][], int delrow[], int delcol[]) {
-        vis[row][col] = 1; 
-        int n = mat.length;
-        int m = mat[0].length;
-        
-        // check for top, right, bottom, left 
-        for(int i = 0;i<4;i++) {
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i]; 
-            // check for valid coordinates and unvisited Os
-            if(nrow >=0 && nrow <n && ncol >= 0 && ncol < m 
-            && vis[nrow][ncol] == 0 && mat[nrow][ncol] == 'O') {
-                dfs(nrow, ncol, vis, mat, delrow, delcol); 
+    public char[][] fill(int n, int m, char[][] mat) {
+        boolean[][] vis = new boolean[n][m];
+
+        // Doing DFS for 'O's in the first row
+        for (int i = 0; i < m; i++) {
+            if (!vis[0][i] && mat[0][i] == 'O') {
+                dfs(0, i, mat, vis);
+            }
+        }
+
+        // Doing DFS for 'O's in the last row
+        for (int i = 0; i < m; i++) {
+            if (!vis[n - 1][i] && mat[n - 1][i] == 'O') {
+                dfs(n - 1, i, mat, vis);
+            }
+        }
+
+        // Doing DFS for 'O's in the first column
+        for (int i = 0; i < n; i++) {
+            if (!vis[i][0] && mat[i][0] == 'O') {
+                dfs(i, 0, mat, vis);
+            }
+        }
+
+        // Doing DFS for 'O's in the last column
+        for (int i = 0; i < n; i++) {
+            if (!vis[i][m - 1] && mat[i][m - 1] == 'O') {
+                dfs(i, m - 1, mat, vis);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 'O' && !vis[i][j]) {
+                    mat[i][j] = 'X';
+                }
+            }
+        }
+
+        return mat;
+    }
+
+    private void dfs(int i, int j, char[][] mat, boolean[][] vis) {
+        vis[i][j] = true;
+        for (int k = 0; k < 4; k++) {
+            if (isSafe(i + dx[k], j + dy[k], mat, vis)) {
+                dfs(i + dx[k], j + dy[k], mat, vis);
             }
         }
     }
 
-    static char[][] fill(int n, int m, char mat[][])
-    {
-        int delrow[] = {-1, 0, +1, 0};
-        int delcol[] = {0, 1, 0, -1}; 
-        int vis[][] = new int[n][m]; 
-        // traverse first row and last row 
-        for(int j = 0 ; j<m;j++) {
-            // check for unvisited Os in the boundary rws
-            // first row 
-            if(vis[0][j] == 0 && mat[0][j] == 'O') {
-                dfs(0, j, vis, mat, delrow, delcol); 
-            }
-            
-            // last row 
-            if(vis[n-1][j] == 0 && mat[n-1][j] == 'O') {
-                dfs(n-1,j,vis,mat, delrow, delcol); 
-            }
-        }
-        
-        for(int i = 0;i<n;i++) {
-            // check for unvisited Os in the boundary columns
-            // first column 
-            if(vis[i][0] == 0 && mat[i][0] == 'O') {
-                dfs(i, 0, vis, mat, delrow, delcol); 
-            }
-            
-            // last column
-            if(vis[i][m-1] == 0 && mat[i][m-1] == 'O') {
-                dfs(i, m-1, vis, mat, delrow, delcol); 
-            }
-        }
-        
-        // if unvisited O then convert to X
-        for(int i = 0;i<n;i++) {
-            for(int j= 0 ;j<m;j++) {
-                if(vis[i][j] == 0 && mat[i][j] == 'O') 
-                    mat[i][j] = 'X'; 
-            }
-        }
-        
-        return mat;
+    private boolean isSafe(int i, int j, char[][] mat, boolean[][] vis) {
+        return (i >= 0 && j >= 0 && i < mat.length && j < mat[0].length && !vis[i][j] && mat[i][j] == 'O');
     }
 }
