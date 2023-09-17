518bfb5d6f8e31e8e75bc83abcb13de6de6b8138
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-17 20:08:00 +0530

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class DriverClass
+{
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int V = Integer.parseInt(str[0]);
+            int E = Integer.parseInt(str[1]);
+    
+            ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
+            for(int i=0;i<V;i++)
+            {
+                adj.add(new ArrayList<ArrayList<Integer>>());
+            }
+            
+            int i=0;
+            while (i++<E) {
+                String S[] = read.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                int w = Integer.parseInt(S[2]);
+                ArrayList<Integer> t1 = new ArrayList<Integer>();
+                ArrayList<Integer> t2 = new ArrayList<Integer>();
+                t1.add(v);
+                t1.add(w);
+                t2.add(u);
+                t2.add(w);
+                adj.get(u).add(t1);
+                adj.get(v).add(t2);
+            }
+            
+            int S = Integer.parseInt(read.readLine());
+            
+            Solution ob = new Solution();
+            
+            int[] ptr = ob.dijkstra(V, adj, S);
+            
+            for(i=0; i<V; i++)
+                System.out.print(ptr[i] + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+   static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
+    {
+        int[]dist=new int[adj.size()];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        boolean[]fin=new boolean[adj.size()];
+        dist[S]=0;
+        for(int i=0;i<V-1;i++){
+            int source=-1;
+            for(int j=0;j<V;j++){
+                if(fin[j]==false&&(source==-1||dist[j]<dist[source])){
+                    source=j;
+                }
+            }
+            fin[source]=true;
+            for(int j=0;j<adj.get(source).size();j++){
+                int node=adj.get(source).get(j).get(0);
+                int weight=adj.get(source).get(j).get(1);
+                if(fin[node]==false){
+                    dist[node]=Math.min(dist[node],dist[source]+weight);
+                }
+            }
+        }
+        return dist;
+    }
+}
+
