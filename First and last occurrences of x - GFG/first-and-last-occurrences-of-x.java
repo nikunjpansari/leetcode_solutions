b4a86ad97f10d78b6f2cfc4f564d9c59872a8115
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-22 15:25:30 +0530

    Added solution - LeetHub

--- First and last occurrences of x - GFG/first-and-last-occurrences-of-x.java
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.java
@@ -13,30 +13,34 @@ import java.util.*;
 
 class GFG
 {
-    ArrayList<Integer> find(int arr[], int n, int target)
+    ArrayList<Integer> find(int arr[], int n, int x)
     {
-       ArrayList<Integer> res = new ArrayList<Integer>();
-        res.add(func(arr,target,true));
-        res.add(func(arr,target,false));
+        ArrayList<Integer> res = new ArrayList<>();
+        res.add(solve(arr,x,true));
+        res.add(solve(arr,x,false));
+        
         return res;
-    }
-     public  static int  func(int[] arr , int target, boolean indx){
+    } 
+    public int solve(int arr[],int x,boolean indx)
+    {
         int ans = -1;
         int l = 0;
-        int r = arr.length-1;
-        while(l<=r){
+        int r = arr.length - 1;
+        
+        while(l <= r)
+        {
             int mid = l + (r-l)/2;
-            if(target<arr[mid]){
-                r = mid-1;
-            } else if (target>arr[mid]){
-                l = mid +1;
-            } else {
-                ans = mid;
-                if (indx){
-                    r = mid -1;
-                }else {
-                    l = mid + 1;
-                }
+            if(x < arr[mid])
+                r = mid - 1;
+            else if(x > arr[mid])
+                l = mid + 1;
+            else
+            {
+                ans = mid ;
+                if(indx)
+                    r = mid - 1;
+                else
+                    l = mid +1;
             }
         }
         return ans;
