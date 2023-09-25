33102c5cf1ea20582b996ea79fc4997e3f8be52a
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-25 15:51:58 +0530

    Added solution - LeetHub

---
+++ Check if two Nodes are Cousins - GFG/check-if-two-nodes-are-cousins.java
@@ -0,0 +1,147 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws Exception{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+            //Scanner sc = new Scanner(System.in);
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+        	    String X = br.readLine();
+        	    String arr[] = X.split(" ");
+        	    int x , y;
+			    x = Integer.parseInt(arr[0]);
+			    y = Integer.parseInt(arr[1]);
+			    if(g.isCousins(root,x,y))
+			        System.out.println(1);
+			     else
+			        System.out.println(0);
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+class Solution 
+{
+    Node xParent = null;
+    Node yParent = null;
+
+    int xDepth = -1 , yDepth =-1;
+    public boolean isCousins(Node root, int x, int y) {
+    
+        solve(root,x,y,0,null);
+        return xDepth==yDepth && xParent != yParent ? true : false;
+    }
+    public void solve(Node root,int x ,int y,int depth,Node parent)
+    {
+        if(root==null)
+        return;
+
+        if(root.data == x)
+        {
+            xParent=parent;
+            xDepth=depth;
+        }
+        else if(root.data == y)
+        {
+            yParent=parent;
+            yDepth=depth;
+        }
+
+        solve(root.left,x,y,depth+1,root);
+        solve(root.right,x,y,depth+1,root);
+    }
+
+}
