54979d5024898630f6dd1e882b54c7fb1cdc6610
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-28 20:50:30 +0530

    Added solution - LeetHub

---
+++ Find the closest number - GFG/find-the-closest-number.java
@@ -0,0 +1,72 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG {
+	public static void main (String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String []element = line1.trim().split(" ");
+		    int n = Integer.parseInt(element[0]);
+		    int target = Integer.parseInt(element[1]);
+		    
+		    int arr[] = new int[n];
+		    
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split(" ");
+		    
+		    for(int index = 0;index < n; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    System.out.println(obj.findClosest(arr, n, target));
+		        
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+    public static int findClosest(int arr[], int n, int target) 
+    { 
+        int i=0,j=n-1;
+        while(j-i>=1)
+        {
+            if(Math.abs(target-arr[i])>Math.abs(arr[j]-target))
+            {
+                i++;
+            }
+            
+            else if(Math.abs(target-arr[i])==Math.abs(arr[j]-target)   && target-arr[i]>target-arr[j])
+            {
+            
+                i++;
+            }
+            else
+            j--;
+        }
+        
+        return arr[j];
+    } 
+}
+
