03029b6797fa0932bc25d15f63bb30abdd17e992
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-09-30 01:08:43 +0530

    Added solution - LeetHub

--- Boolean Matrix - GFG/boolean-matrix.java
+++ Boolean Matrix - GFG/boolean-matrix.java
@@ -42,33 +42,41 @@ class GFG
 
 // } Driver Code Ends
 
+
+//User function Template for Java
+
 class Solution
 {
     void booleanMatrix(int matrix[][])
     {
-        int m=matrix.length;
-        int n=matrix[0].length;
-        
-        int row[]=new int[m];
-        Arrays.fill(row,-1);
-        
-        int col[]=new int[n];
-        Arrays.fill(col,-1);
-        
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(matrix[i][j]==1){
-                    row[i]=1;
-                    col[j]=1;
-                }
-            }
-        }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i]==1 || col[j]==1){
-                    matrix[i][j]=1;
-                }
-            }
-        }
+      int n = matrix.length;
+      int m = matrix[0].length;
+      
+      int[] row = new int[n];
+      Arrays.fill(row,-1);
+      
+      int[] col = new int[m];
+      Arrays.fill(col,-1);
+      
+      for(int i=0;i<n;i++)
+      {
+          for(int j =0 ;j<m;j++)
+          {
+              if(matrix[i][j]==1)
+              {
+                  row[i] = 1;
+                  col[j] = 1;
+              }
+
+          }
+      }
+      for(int i=0;i<n;i++)
+      {
+          for(int j =0 ;j<m;j++)
+          {
+              if( row[i] == 1 || col[j] == 1)
+              matrix[i][j]=1;
+          }
+      }
     }
 }
