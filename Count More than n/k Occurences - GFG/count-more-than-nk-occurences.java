f521c7818af4f003675f0fd7f6898c2357eb7697
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-05 13:07:58 +0530

    Added solution - LeetHub

---
+++ Count More than n/k Occurences - GFG/count-more-than-nk-occurences.java
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class Driverclass {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+
+            System.out.println(new Solution().countOccurence(arr, n, k));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution 
+{
+    public int countOccurence(int[] arr, int n, int k) 
+    {
+        int times=n/k;
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        
+        for(int x:arr)
+           hm.put(x,hm.getOrDefault(x,0)+1);
+        
+        
+        int count=0;
+        
+        for(Map.Entry<Integer,Integer> m:hm.entrySet())
+        {
+            if(m.getValue()>times)
+              count++;
+        }
+        return count;
+    }
+}
