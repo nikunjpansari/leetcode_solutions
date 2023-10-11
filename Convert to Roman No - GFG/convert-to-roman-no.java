6efc296c0285d618115c356432d5a1bdeb26c423
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-11 16:02:37 +0530

    Added solution - LeetHub

---
+++ Convert to Roman No - GFG/convert-to-roman-no.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.util.Scanner;
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int N = sc.nextInt();
+			
+			Solution ob = new Solution();
+			System.out.println(ob.convertToRoman(N));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+	String convertToRoman(int A) {
+	 int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+    String[] strs = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"}; 
+    
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i = 0 ; i<values.length;i++)
+    {
+        while(A >= values[i])
+        {
+            A -= values[i];
+            sb.append(strs[i]);
+        }
+    }
+    return sb.toString();
+	}
+}
