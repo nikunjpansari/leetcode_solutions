83c79450faa054b4064be9432a790a0228d6ebb5
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-13 10:32:06 +0530

    Added solution - LeetHub

---
+++ Min Cost Climbing Stairs - GFG/min-cost-climbing-stairs.java
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            
+            String S[] = read.readLine().split(" ");
+            
+            int[] cost = new int[N];
+            
+            for(int i=0 ; i<N ; i++)
+                cost[i] = Integer.parseInt(S[i]);
+
+            Solution ob = new Solution();
+            System.out.println(ob.minCostClimbingStairs(cost,N));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//Back-end complete function Template for Java
+
+class Solution {
+    static int minCostClimbingStairs(int[] cost , int n) {
+        for(int i =2;i<n;i++)
+        cost[i] = Math.min(cost[i-1],cost[i-2]) + cost[i];
+        
+        return Math.min(cost[n-1],cost[n-2]);
+    }
+};
