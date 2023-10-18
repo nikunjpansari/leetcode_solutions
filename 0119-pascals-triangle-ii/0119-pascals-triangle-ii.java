6680447cc77652aecbcefd6c1519d46f502132af
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-18 12:08:33 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (97.21%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -1,19 +1,14 @@
-// Time Complexity : O(k)
-// Space Complexity : O(k)
 class Solution {
-    public List<Integer> getRow(int k) 
-    {
-        List<Integer> row = new ArrayList<>();
-
+    public List<Integer> getRow(int k) {
+        List<Integer> res = new ArrayList<>();
         long ans = 1;
-        row.add((int) ans); 
-        
-        for (int i = 1; i <= k; i++) 
-        {
-            ans = ans * (k - i + 1) / i;
-            row.add((int) ans);
-        }
+        res.add((int) ans);
 
-        return row;
+        for(int i = 1 ; i<=k;i++)
+        {
+            ans = ans * (k-i+1)/i;
+            res.add((int) ans);        
+        }
+        return res;
     }
 }
