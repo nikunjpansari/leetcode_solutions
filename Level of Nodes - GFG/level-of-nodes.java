c8c79eb4a847cda4ca37aa3c12bc950c11bbe0e1
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-19 19:24:59 +0530

    Added solution - LeetHub

--- Level of Nodes - GFG/level-of-nodes.java
+++ Level of Nodes - GFG/level-of-nodes.java
@@ -32,40 +32,37 @@ class DriverClass
     }
 }
 // } Driver Code Ends
-
 class Solution
 {
-    int nodeLevel(int V, ArrayList<ArrayList<Integer>> adj, int X) 
+    int nodeLevel(int V, ArrayList<ArrayList<Integer>> adj, int X)
     {
-        boolean[] vis= new boolean[V];
+        boolean[] vis = new boolean[V];
         
-        Queue<Integer> q= new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         q.add(0);
-        int l=0;
+        int l = 0;
         
         while(!q.isEmpty())
         {
-            int s=q.size();
-            
-            while(s!=0)
+            int s = q.size();
+            while(s != 0)
             {
+            int x = q.poll();
+            
+            if(x == X)
+            return l;
+            
+            vis[x] = true;
+            
+            for(int k : adj.get(x))
+            {
+            if(vis[k] == false)
+            q.add(k);
+            }
+            s--;
                 
-                int x=q.poll();
-                
-                if(x==X) 
-                return l;
-                
-                vis[x]=true;
-                
-                for(int k: adj.get(x))
-                {
-                    if(vis[k]==false) 
-                    q.add(k);
-                }
-                s--;
             }
             l++;
-            
         }
         return -1;
     }
