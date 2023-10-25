216d8f7c66cf563fba8d22d6f39c9afd5d8c3c7a
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-25 09:55:06 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -28,28 +28,26 @@ class GFG{
     }
 }
 // } Driver Code Ends
-
 class Solution{
-    static int knapSack(int n, int W, int val[], int wt[])
+    static int knapSack(int N, int W, int val[], int wt[])
     {
-           int[][] dp = new int[n+1][W+1];
-    
-        for(int i = 0 ; i< dp.length; i++)
-            dp[i][0] = 0;
-        
-        for(int j =0 ; j< dp[0].length; j++)
-            dp[0][j] = 0;        
-        
-        for(int i = 1 ; i < n + 1 ; i++)
-        {
-            for(int j = 1; j < W+1 ; j++)
-            {
-                if(wt[i-1] <= j)
-                    dp[i][j] = Math.max(val[i-1] + dp[i][j- wt[i-1]], dp[i-1][j]);
-                else
-                    dp[i][j] = dp[i-1][j];
-            }
-        }
-        return dp[n][W];
+      int[][] dp = new int[N+1][W+1];
+      for(int i = 0 ;i < dp.length;i++)
+      dp[i][0] = 0;
+      
+      for(int j = 0; j<dp[0].length;j++)
+      dp[0][j] = 0;
+      
+      for(int i = 1 ; i < N+1 ; i++)
+      {
+          for(int j = 1;j<W+1;j++)
+          {
+              if(wt[i-1] <= j)
+              dp[i][j] = Math.max(val[i-1] + dp[i][j-wt[i-1]],dp[i-1][j]);
+              else
+            dp[i][j] = dp[i-1][j];
+          }
+      }
+      return dp[N][W];
     }
 }
