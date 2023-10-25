1f2be3ab4a2f076c213b9f5594a0297c85a251d5
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-10-25 12:14:03 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.java
+++ Coin Change - GFG/coin-change.java
@@ -21,31 +21,27 @@ class GfG {
 
 // } Driver Code Ends
 
-// Time Complexity : O(n * sum)
-// Space Complexity : O(n * sum)
-// User function Template for Java
-
 class Solution {
-      public long count(int[] coins, int n, int sum) {
-        long[][] dp = new long[n + 1][sum + 1];
-
-        for (int i = 0; i <= sum; i++) 
-            dp[0][i] = 0;
-        
-        for (int i = 0; i <= n; i++) 
-            dp[i][0] = 1;
-        
-        for (int i = 1; i <= n; i++) 
+    public long count(int coins[], int N, int sum) {
+       long[][] dp = new long[N+1][sum+1];
+       
+       for(int i = 0 ; i<N+1;i++)
+       dp[i][0] = 1;
+       
+       for(int j=0;j<sum+1;j++)
+       dp[0][j] = 0;
+       
+       for(int i = 1 ; i < N+1;i++)
+       {
+       for(int j = 1 ; j < sum +1;j++)
         {
-            for (int j = 1; j <= sum; j++) 
-            {
-                if (coins[i - 1] > j) 
-                    dp[i][j] = dp[i - 1][j];
-                else 
-                    dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
-            }
-        }
-
-        return dp[n][sum];
+            if(coins[i-1] <= j )
+            dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+            else
+            dp[i][j] = dp[i-1][j];
+            
+        }       
+       }
+       return dp[N][sum];
     }
 }
