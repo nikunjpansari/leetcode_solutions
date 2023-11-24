fa3ee63d1c9d511961a680b28fc866f465b92d44
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-11-24 14:35:34 +0530

    Added solution - LeetHub

---
+++ Maximum Number of coins - GFG/maximum-number-of-coins.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG{
+	public static void main(String [] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int test = Integer.parseInt(br.readLine());
+		while(test-- > 0) {
+			int N = Integer.parseInt(br.readLine());
+			ArrayList<Integer> arr = new ArrayList<>(N);
+			String [] str = br.readLine().trim().split(" ");
+			for(int i = 0; i < N; i++) {
+				arr.add(Integer.parseInt(str[i]));
+			}
+			Solution obj = new Solution();
+			System.out.println(obj.maxCoins(N, arr));
+		}
+	}
+}
+// } Driver Code Ends
+class Solution{
+     int maxCoins(int N, ArrayList<Integer> a) 
+     {
+	    int n = a.size();
+    
+        a.add(0, 1);
+        a.add(1);
+        
+        int[][] dp = new int[n + 2][n + 2];
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n; j++) {
+                if (i > j) continue;
+                int maxi = Integer.MIN_VALUE;
+                
+
+                for (int ind = i; ind <= j; ind++) {
+                    int cost = a.get(i - 1) * a.get(ind) * a.get(j + 1) +
+                               dp[i][ind - 1] + dp[ind + 1][j];
+                    maxi = Math.max(maxi, cost);
+                }
+                dp[i][j] = maxi;
+            }
+        }
+        return dp[1][n];
+	}
+}
