90dcb1b5c03922268911e643ec581b184a71f09f
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2023-12-06 10:21:16 +0530

    Time: 1 ms (60.95%), Space: 39.7 MB (13.57%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int totalMoney(int n) {
+        int sum = 0 ;
+        for(int i =0 ;i<n;i++)
+        {
+            int a =  i/7;
+            int rem = i % 7;
+            sum += a +rem +1;
+        }
+        return sum;
+    }
+}
