5aeb8355d669c2b1060b13f31cba7ed3c98f7f1c
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-01-21 10:36:40 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (56.14%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-       int a = 0;
-       int b = 0;
-
+        int a = 0 , b = 0;
         for(int i : nums)
         {
-            int a1 = b + i;
-            int b1 = Math.max(a,b);
+        int a1 = b+i;
+        int b1 = Math.max(a,b);
 
-            a = a1;
-            b = b1;
-        } 
-        return Math.max(a,b);
+        a = a1;
+        b = b1; 
+    }
+    return Math.max(a,b);
     }
 }
