80f7598a93a4baf5265fa0df86aa14910a9a2d02
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-03-06 10:05:52 +0530

    Time: 0 ms (100.00%), Space: 44.9 MB (5.57%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -3,14 +3,14 @@ public class Solution {
         if(head == null)
         return false;
 
-        ListNode p1 = head , p2 = head;
-        while(p1!=null && p1.next!=null)
+        ListNode p1 = head, p2= head;
+        while(p2!= null && p2.next != null)
         {
-            p1 = p1.next.next;
-            p2 = p2.next;
+        p1 = p1.next;
+        p2 = p2.next.next;
 
-            if(p1 == p2)
-            return true;
+        if(p1 == p2)
+        return true;
         }
         return false;
     }
