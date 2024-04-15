b1bca79129405e9afc167a29d2fde10f61340003
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-04-15 10:53:04 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (86.84%) - LeetHub

---
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+       return sum(root,0); 
+    }
+    public int sum(TreeNode root, int s)
+    {
+        if(root == null)
+        return 0;
+
+        if(root.left == null && root.right == null)
+        return s*10 + root.val;
+
+        return sum(root.left,s*10+root.val) + sum(root.right,s*10+root.val);
+    }
+}
