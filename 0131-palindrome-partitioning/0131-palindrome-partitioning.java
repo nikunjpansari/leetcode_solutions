e8c3e4a1ee0b346bf583b689c1e3670e24c5a585
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-05-22 08:58:26 +0530

    Time: 8 ms (60.77%), Space: 56.8 MB (57.79%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,29 +1,34 @@
 class Solution {
-     public List<List<String>> partition(String s) {
-        List<List<String>> res= new ArrayList<>(); 
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
         List<String> path = new ArrayList<>();
-        func(0, s, path, res);
+        func(0,s,path,res);
         return res;
     }
-    
-    void func(int index, String s, List<String> path, List<List<String>> res) {
-        if(index == s.length()) {
+    void func(int index,String s,List<String> path,List<List<String>> res)
+    {
+        if(index == s.length())
+        {
             res.add(new ArrayList<>(path));
             return;
         }
-        for(int i = index; i < s.length(); ++i) {
-            if(isPalindrome(s, index, i)) {
-                path.add(s.substring(index, i+1));
-                func(i+1, s, path, res);
-                path.remove(path.size()-1);
+
+        for(int i = index;i<s.length();++i)
+        {
+            if(isPalindrome(s,index,i))
+            {
+            path.add(s.substring(index,i+1));
+            func(i+1,s,path,res);
+            path.remove(path.size()-1);
             }
         }
     }
-    
-    boolean isPalindrome(String s, int start, int end) {
-        while(start <= end) {
-            if(s.charAt(start++) != s.charAt(end--))
-                return false;
+    boolean isPalindrome(String s, int st ,int e)
+    {
+        while(st<=e)
+        {
+        if(s.charAt(st++) != s.charAt(e--))
+        return false;
         }
         return true;
     }
