7e6200544987171fc83de021dd6f355d790c716f
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-05-31 10:52:03 +0530

    Time: 1 ms (100.00%), Space: 45.7 MB (22.05%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.java
+++ 0260-single-number-iii/0260-single-number-iii.java
@@ -12,11 +12,10 @@ class Solution {
 
         int num1 = 0, num2 = 0;
         for (int num : nums) {
-            if ((num & rightmostSetBit) == 0) {
+            if ((num & rightmostSetBit) == 0)
                 num1 ^= num;
-            } else {
+            else 
                 num2 ^= num;
-            }
         }
 
         return new int[]{num1, num2};
