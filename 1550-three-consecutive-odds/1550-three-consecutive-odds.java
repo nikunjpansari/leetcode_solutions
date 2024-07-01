51b0416d6df235498b655edb38da6381ad9772dc
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-07-01 08:53:33 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (42.33%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.java
@@ -0,0 +1,13 @@
+class Solution {
+        public boolean threeConsecutiveOdds(int[] arr) 
+        {
+        for (int i = 0, cnt = 0; i < arr.length; i++) 
+        {
+            if (arr[i] % 2 == 0) 
+            cnt = 0;
+            else if (++cnt == 3) 
+            return true;
+        }
+        return false;
+    }
+}
