e3bb6524959e75e9367541cc8b15ac439cc8e167
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2024-09-08 09:07:03 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (34.41%) - LeetHub

---
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] parts = new ListNode[k];
+        int len = 0; 
+        ListNode node = head;
+        while(node !=null)
+        {
+            len+=1;
+            node=node.next;
+        }
+        int n=len/k;
+        int r=len%k;
+        node=head;
+        ListNode prev =  null;
+        for(int i=0;node!=null && i<k;i++,r--)
+        {
+            parts[i]=node;
+            for(int j=0;j<n + (r>0 ?1:0);j++)
+            {
+                prev=node;
+                node=node.next;
+            }
+            if(prev!=null)
+            prev.next=null;
+        }
+
+        return parts;  
+         }
+}
