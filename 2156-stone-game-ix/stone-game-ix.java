e96722a1669557e18299a1ad652744b789b17631
NIKUNJ PANSARI <nikunjpansari95@gmail.com>
2026-08-16 10:56:02 +0530

    Time: 3 ms (100.00%) | Memory: 114.8 MB (64.86%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int[] count = new int[3];
+
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
